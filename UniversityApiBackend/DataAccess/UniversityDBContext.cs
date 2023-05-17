822ca2a08178aebf299361574049fd95ac91ab38
Tapia <burbujeuky@gmail.com>
2023-05-17 01:16:26 -0400

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        {
+
+        }
+       
+
+        //TODO: Add DbSets (Tables of our DataBase)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
---
+++ UniversityApiBackend/Models/DataModels/Curso.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Curso: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Nombre { get; set; } = string.Empty;
+        
+        [Required, StringLength(280)]
+        public string DescripcionCorta { get; set; } = string.Empty;
+        
+        [Required, StringLength(350)]
+        public string DescripcionLarga { get; set; } = string.Empty;
+        
+        [Required, StringLength(50)]
+        public string PublicoObjetivo { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        public string Objetivos { get; set; } = string.Empty;
+
+        [Required, StringLength(50)]
+        public string Requisitos { get; set; } = string.Empty;
+
+        [EnumDataType(typeof(NivelCurso))]
+        public NivelCurso Nivel { get; set; }
+    }
+
+    public enum NivelCurso
+    {
+        [Display(Name = "Básico")]
+        Basico,
+
+        [Display(Name = "Intermedio")]
+        Intermedio,
+
+        [Display(Name = "Avanzado")]
+        Avanzado
+    }
+}
